51b458fa0f5c3bc87cb20a12a3645cf01698089d
febittencourt <fernanda.bvalle@hotmail.com>
2021-06-20 22:01:43 -0300

    projeto-quinzena3

---
+++ quinzena2/loops/index.js
@@ -0,0 +1,21 @@
+// Exercícios de interpretação de código
+
+
+//Exercício 1:
+
+// o código está adicionando 1 à variável valor, até que a variável i chegue em 4. Dessa forma, no console será impresso 10.
+
+//Exercício 2:
+
+// a. [19, 21, 23, 25, 27, 30]
+// b. O for...of... seria suficiente, mas teria que inserir uma variável no escopo pai: let indice = 0
+// escrever o código e fora do escopo filho, ir adicionando 1 no indice: indice++
+
+
+//Exercício 3:
+
+// resultado no console:
+// *
+// **
+// ***
+// ****
